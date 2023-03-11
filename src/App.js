163950b6cfcdac39ca9dcb9b38a6947286bb94d3
Amisha <amishashrivastava998@gmail.com>
2023-03-11 23:12:42 +0530

    scrollable

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
 import SearchBox from './SearchBox'; 
+import Scroll from './Scroll';
 import './App.css';
 
 
@@ -10,9 +11,16 @@ class App extends Component {
     constructor() {
         super()
         this.state = {
-        robots: robots,
+        robots: [],
         searchField:''
         }
+    }
+
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> response.json())
+        .then(users=>{this.setState({robots:users})});
     }
 
     onSearchChange = (event) => {
@@ -29,7 +37,9 @@ class App extends Component {
     <div className='tc'>
         <h1 className='f1'>RoboFriends</h1>
         <SearchBox searchChange = {this.onSearchChange}/>
-        <CardList  robots = {filteredRobots}/> 
+        <Scroll>
+            <CardList  robots = {filteredRobots}/> 
+        </Scroll>
     </div> 
     );
     }
